81133924ff6e5a521f4270b2f8e9c3e159079a7a
Adi Zaenul Mustaqim <adi.mustaqim@students.amikom.ac.id>
2021-12-08 12:35:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# load eksternal model
+filename = 'model.sav'
+model = load(filename)
+
 # definisi hasil klasifikasi
 def HasilKlasifikasi(hasil):
     jenisIris = None
@@ -17,9 +21,15 @@ def HasilKlasifikasi(hasil):
     
     return jenisIris
 
+# route untuk menampilkan halaman awal (index.html)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 # route tampilan awal
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/getResult', methods=['POST'])
+def getResult():
+
     if (request.method == 'POST'):
         # mendefinisikan list untuk menampung data yang akan diinput user
         singleData = []
@@ -27,7 +37,10 @@ def index():
         # mendefinisikan list untuk menampung data
         dataTest = []
 
-        singleData = [x for x in request.form.values()]
+        # input data
+        for x in range(4):
+            data = request.json[x]
+            singleData.append(float(data))
 
         # menggabungkan tiap single data ke dalam data test
         dataTest.append(singleData)
@@ -39,16 +52,10 @@ def index():
         scaler = load('scaler.bin')
         dataTest = scaler.transform(dataTest)
 
-        # load eksternal model
-        filename = 'model.sav'
-        model = load(filename)
-
         # melakukan prediksi data baru
         prediksi = int(model.predict(dataTest))
         
-        return render_template('index.html', prediction = 'test')
-
-    return render_template('index.html')
+        return render_template('index.html', prediction = HasilKlasifikasi(prediksi))
 
 if __name__ == '__main__':
     app.run(debug=True)
