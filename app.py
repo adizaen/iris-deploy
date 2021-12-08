a44b5fde09c5394d35d9b4ef904227c7e3cbf798
Adi Zaenul Mustaqim <adi.mustaqim@students.amikom.ac.id>
2021-12-08 11:03:09 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+import numpy as np
+from joblib import load
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# definisi hasil klasifikasi
+def HasilKlasifikasi(hasil):
+    jenisIris = None
+    
+    if hasil == 0:
+        jenisIris = 'Iris Setosa'
+    elif hasil == 1:
+       jenisIris = 'Iris Versicolor'
+    else:
+       jenisIris = 'Iris Virginica'
+    
+    return jenisIris
+
+# route tampilan awal
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if (request.method == 'POST'):
+        # mendefinisikan list untuk menampung data yang akan diinput user
+        singleData = []
+
+        # mendefinisikan list untuk menampung data
+        dataTest = []
+
+        # perulangan untuk memsukan data
+        # angka 4 -> jumlah kolom
+        for x in range(4):
+            data = request.json[x]
+            data = float(data)
+            singleData.append(data)
+
+        # menggabungkan tiap single data ke dalam data test
+        dataTest.append(singleData)
+
+        # convert dari list ke numpy array
+        dataTest = np.array(dataTest)
+
+        # load file scaler
+        scaler = load('scaler.bin')
+        dataTest = scaler.transform(dataTest)
+
+        # load eksternal model
+        filename = 'model.sav'
+        model = load(filename)
+
+        # melakukan prediksi data baru
+        prediksi = int(model.predict(dataTest))
+        
+        return render_template('index.html', prediction = HasilKlasifikasi(prediksi))
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
