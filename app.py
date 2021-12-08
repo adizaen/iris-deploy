affe78c5a52c14bd3498092077cbbcc75e7e2d22
Adi Zaenul Mustaqim <adi.mustaqim@students.amikom.ac.id>
2021-12-08 12:30:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def index():
         # melakukan prediksi data baru
         prediksi = int(model.predict(dataTest))
         
-        return render_template('index.html', prediction = prediksi)
+        return render_template('index.html', prediction = 'test')
 
     return render_template('index.html')
 
